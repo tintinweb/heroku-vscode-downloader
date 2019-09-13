147730df44857f40c060bba2c1caaaf0ff4a77c3
tintinweb <tintinweb@oststrom.com>
2019-09-14 00:39:46 +0200

    v0.1

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 import os
-from flask import Flask
+from flask import Flask, request, redirect, render_template
 
 import requests
+from urlparse import urlparse
+import re
+
+class VscodeExtensionUrl:
+
+    DOWNLOAD_URL = "https://ms-vscode.gallery.vsassets.io/_apis/public/gallery/publisher/%(publisher)s/extension/%(name)s/%(version)s/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+
+    @staticmethod
+    def download(marketplace_url):
+        url = urlparse(marketplace_url)
+        if url.netloc!="marketplace.visualstudio.com" or not url.query:
+            raise Exception("not a marketplace url")
+
+        params = {}
+        for kv in url.query.strip().split("&"):
+            k, v = kv.split("=")
+            params[k] = v
+
+        itemName = params.get("itemName")
+        if not itemName:
+            raise Exception("not a marketplace url - missing extension name")
+
+        publisher, extension = itemName.strip().split(".")
+
+        version = re.findall(r'"version":"([^"]+)"',requests.get(marketplace_url).text)
+
+        if not len(version):
+            raise Exception("not a marketplace url - missing version")
+
+        return VscodeExtensionUrl.DOWNLOAD_URL % {"publisher":publisher, "name":extension, "version": version.pop(0)}
 
 # initialization
 app = Flask(__name__)
@@ -12,7 +42,14 @@ app.config.update(
 # controllers
 @app.route("/")
 def hello():
-    return "Hello from Python!" + requests.get("http://www.myip.ch/").text
+    url = request.args.get('url')
+    if url:
+        return redirect(VscodeExtensionUrl.download(url), code=302)
+
+    return render_template('index.html')
+
+
+
 
 # launch
 if __name__ == "__main__":
